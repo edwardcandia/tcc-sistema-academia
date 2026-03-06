95cd7a306690a2bb00e67b3a52be5d54bc18c65f
amaral1703 <gamaral987654321@gmail.com>
2026-03-06 17:52:45 -0300

    feat: implementar funcionalidade de reset de senha para alunos, utilizando CPF como senha padrão, tirando a possibilidade de entrada como senha nula

--- backend/src/routes/alunosRoutes.ts
+++ backend/src/routes/alunosRoutes.ts
@@ -10,6 +10,9 @@ router.get('/alunos', authenticateUser, authorizeRoles(['administrador', 'atende
 router.post('/alunos', authenticateUser, authorizeRoles(['administrador', 'atendente']), alunosController.createAluno);
 router.put('/alunos/:id', authenticateUser, authorizeRoles(['administrador', 'atendente']), alunosController.updateAluno);
 
+// Reset de senha do aluno
+router.patch('/alunos/:id/reset-senha', authenticateUser, authorizeRoles(['administrador', 'atendente']), alunosController.resetSenhaAluno);
+
 // Apenas administradores podem excluir alunos
 router.delete('/alunos/:id', authenticateUser, authorizeRoles(['administrador']), alunosController.deleteAluno);
 
